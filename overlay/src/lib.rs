477e314daaff227d5445324bc4b7be5c783b1359
hananel42 <chnanel.pedahel@gmail.com>
2026-05-24 22:39:22 +0300

    delete unused files

--- overlay/src/lib.rs
+++ overlay/src/lib.rs
@@ -1,8 +1,6 @@
 pub mod canvas;
 pub mod capture;
 pub mod state;
-mod text_engine;
-pub mod ui;
 mod win32;
 
 pub use win32::{EventResult, MouseButton, OverlayApp, OverlayContext, OverlayEvent, run};
