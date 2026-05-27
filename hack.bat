78f764a62166f33b1dc1a37fbd0e77adf050cf12
hananel <chnanel.pedahel@gmail.com>
2026-05-27 13:42:11 +0300

    Modify hack.bat to use dynamic effect selection

--- hack.bat
+++ hack.bat
@@ -1,8 +1,21 @@
 @echo off
+setlocal enabledelayedexpansion
+
 set "REPO=hananel42/screen-tricks"
+set "FLAG=%~1"
+set "EFFECT=particles"
+
+:: בדיקת הדגל שהמשתמש שלח
+if "%FLAG%"=="-w" set "EFFECT=wave"
+if "%FLAG%"=="-t" set "EFFECT=triangulate"
+if "%FLAG%"=="-p" set "EFFECT=particles"
+
+echo Launching %EFFECT%...
 
 powershell -Command ^
-    "$url = (Invoke-RestMethod -Uri 'https://api.github.com/repos/%REPO%/releases/latest').assets | Where-Object { $_.name -like '*.exe' } | Select-Object -ExpandProperty browser_download_url;" ^
-    "Invoke-WebRequest -Uri $url -OutFile 'particles.exe';" ^
-    "Unblock-File -Path '.\particles.exe';" ^
-    "Start-Process '.\particles.exe'"
+    "$url = (Invoke-RestMethod -Uri 'https://api.github.com/repos/%REPO%/releases/latest').assets | Where-Object { $_.name -like '*%EFFECT%*.exe' } | Select-Object -ExpandProperty browser_download_url -First 1;" ^
+    "Invoke-WebRequest -Uri $url -OutFile '%EFFECT%.exe';" ^
+    "Unblock-File -Path '.\%EFFECT%.exe';" ^
+    "Start-Process '.\%EFFECT%.exe'"
+
+endlocal
