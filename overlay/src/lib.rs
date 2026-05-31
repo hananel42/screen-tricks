0abdd9adce9802d869bc3c2c2f3db6a94740e518
hananel42 <204704271+hananel42@users.noreply.github.com>
2026-05-31 16:27:45 +0000

    style: auto-fix Rust code formatting

--- overlay/src/lib.rs
+++ overlay/src/lib.rs
@@ -32,4 +32,6 @@ mod state;
 mod win32;
 
 pub use canvas::Canvas;
-pub use win32::{EventResult, MouseButton, OverlayApp, OverlayContext, OverlayEvent, run,get_width,get_height};
+pub use win32::{
+    EventResult, MouseButton, OverlayApp, OverlayContext, OverlayEvent, get_height, get_width, run,
+};
